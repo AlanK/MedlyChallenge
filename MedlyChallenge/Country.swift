1dd0c3e15f3e669ed9a85a9bb206ca59ebd711b7
Alan Kantz <alan.kantz@gmail.com>
2020-11-20 14:15:17 -0500

    The country properties must be clearly described for future maintainers.

--- MedlyChallenge/Country.swift
+++ MedlyChallenge/Country.swift
@@ -7,10 +7,10 @@
 
 /// A country with a name, flag code, and capital.
 struct Country {
-    
+    /// The name of the country.
     let name: String
-    
+    /// The `alpha2Code` for the country, used to identify the country flag.
     let flagCode: String
-    
+    /// The capital of the country.
     let capital: String
 }
