2a5a61944f22cf3917be90002b60bfff6a844264
Alan Kantz <alan.kantz@gmail.com>
2020-11-20 14:17:22 -0500

    It should be possible to decode a country from JSON, and the JSON does not use exactly the same property names we use.

--- MedlyChallenge/Country.swift
+++ MedlyChallenge/Country.swift
@@ -6,11 +6,17 @@
 //
 
 /// A country with a name, flag code, and capital.
-struct Country {
+struct Country: Decodable {
     /// The name of the country.
     let name: String
     /// The `alpha2Code` for the country, used to identify the country flag.
     let flagCode: String
     /// The capital of the country.
     let capital: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case flagCode = "alpha2Code"
+        case capital
+    }
 }
