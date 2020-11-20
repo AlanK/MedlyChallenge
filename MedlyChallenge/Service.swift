84bc5770f2d825f7ac0a4dd3b341a650ee7373ad
Alan Kantz <alan.kantz@gmail.com>
2020-11-20 16:20:56 -0500

    Refactored to move the guts of most network calls into a protocol extension.

--- MedlyChallenge/Service.swift
+++ MedlyChallenge/Service.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum Service {
+enum Service: ServiceProtocol {
     
     /// Get a list of countries asynchronously.
     /// - Parameter location: Text representing the location where the list of countries is located.
@@ -18,35 +18,88 @@ enum Service {
         guard let url = URL(string: location) else {
             return completionHandler(.failure(RequestError.couldNotCreateURL(input: location)))
         }
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            completionHandler(
-                Result {
-                    if let error = error { throw error }
-                    guard let data = data else { throw ResponseError.dataWasNil }
-                    return try JSONDecoder().decode([Country].self, from: data)
-                }
-            )
-        }
-        .resume()
+        URLSession.shared
+            .dataTask(with: url, completionHandler: finish(with: completionHandler))
+            .resume()
+    }
+}
+
+/// Helper functions for making network calls.
+///
+/// Typical use:
+/// ```swift
+/// enum SomeService: ServiceProtocol {
+///     static func getThing(_ completionHandler: @escaping (Result<Bool, Error>) -> Void) {
+///         let url = // ...
+///         let task = URLSession.shared.dataTask(with: url, completionHandler: finish(with: completionHandler))
+///         task.resume()
+///     }
+/// }
+/// ```
+protocol ServiceProtocol { }
+
+extension ServiceProtocol {
+    
+    /// An adapter for passing a typical network call completion handler directly to a data task initializer.
+    /// - Parameter completionHandler: A completion handler in the form of `(Result<DecodedOutput, Error>) -> Void`.
+    /// - Returns: A function that can be passed to a data task initializer.
+    static func finish<Output: Decodable>(
+        with completionHandler: @escaping (Result<Output, Error>) -> Void
+    ) -> (Data?, URLResponse?, Error?) -> Void {
+        
+        feed(completionHandler, withOutputOf: decode)
     }
     
-    enum RequestError: LocalizedError {
-        case couldNotCreateURL(input: String)
+    /// An adapter for connecting a typical network call completion handler to a decoding function.
+    ///
+    /// If the thing being decoded is `Decodable`, take a look at `finish(with:)`.
+    /// - Parameters:
+    ///   - completion: A completion handler in the form of `(Result<Output, Error>) -> Void`.
+    ///   - responseHandler: A response handler that decodes a network response into a usable value.
+    /// - Returns: A function that can be passed to a data task initializer.
+    static func feed<Output>(
+        _ completion: @escaping (Result<Output, Error>) -> Void,
+        withOutputOf responseHandler: @escaping (Data?, URLResponse?, Error?) throws -> Output
+    ) -> (Data?, URLResponse?, Error?) -> Void {
         
-        var errorDescription: String? {
-            switch self {
-            case let .couldNotCreateURL(input): return "Could not create URL from input string \(input)"
-            }
+        { (data, urlResponse, error) in
+            let result = Result { try responseHandler(data, urlResponse, error) }
+            completion(result)
         }
     }
     
-    enum ResponseError: LocalizedError {
-        case dataWasNil
-        
-        var errorDescription: String? {
-            switch self {
-            case .dataWasNil: return "The data returned by the call was unexpectedly nil."
-            }
+    /// Attempts to decode a value from a network response.
+    /// - Parameters:
+    ///   - data: An optional data object. If this is `nil`, the function will throw.
+    ///   - urlResponse: An optional URL response object.
+    ///   - error: An optional error. If this is not `nil`, it will be thrown.
+    /// - Throws: The provided error, or a `ResponseError`.
+    /// - Returns: A decoded value.
+    static func decode<Output: Decodable>(data: Data?, urlResponse: URLResponse?, error: Error?) throws -> Output {
+        if let error = error { throw error }
+        guard let data = data else { throw ResponseError.dataWasNil }
+        return try JSONDecoder().decode(Output.self, from: data)
+    }
+}
+
+/// An error encountered while making a network request.
+enum RequestError: LocalizedError {
+    case couldNotCreateURL(input: String)
+    
+    var errorDescription: String? {
+        switch self {
+        case let .couldNotCreateURL(input): return "Could not create URL from input string \(input)"
+        }
+    }
+}
+
+/// An error encountered while handling the response to a network request.
+enum ResponseError: LocalizedError {
+    case dataWasNil
+    
+    var errorDescription: String? {
+        switch self {
+        case .dataWasNil: return "The data returned by the call was unexpectedly nil."
         }
     }
 }
