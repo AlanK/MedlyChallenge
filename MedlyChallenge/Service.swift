57b131aa67b13f5c685296cc2ff8ff406da11e7b
Alan Kantz <alan.kantz@gmail.com>
2020-11-20 14:52:45 -0500

    We should be able to substitute a different location for a network request if necessary.

--- MedlyChallenge/Service.swift
+++ MedlyChallenge/Service.swift
@@ -10,12 +10,14 @@ import Foundation
 enum Service {
     
     /// Get a list of countries asynchronously.
+    /// - Parameter location: Text representing the location where the list of countries is located.
     /// - Parameter completionHandler: The function to call with the result of getting the list of countries, either the list or an error.
-    static func getCountries(_ completionHandler: @escaping (Result<[Country], Error>) -> Void) {
-        let resourceLocation = "https://restcountries.eu/rest/v2/all"
-        guard
-            let url = URL(string: resourceLocation)
-        else { return completionHandler(.failure(RequestError.couldNotCreateURL(input: resourceLocation))) }
+    static func getCountries(from location: String = "https://restcountries.eu/rest/v2/all",
+                             _ completionHandler: @escaping (Result<[Country], Error>) -> Void) {
+        
+        guard let url = URL(string: location) else {
+            return completionHandler(.failure(RequestError.couldNotCreateURL(input: location)))
+        }
         URLSession.shared.dataTask(with: url) { data, _, error in
             completionHandler(
                 Result {
