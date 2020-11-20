60c5cc87dc09a7a9b44c98b71199fea8208e744a
Alan Kantz <alan.kantz@gmail.com>
2020-11-20 14:06:10 -0500

    Initial Commit

---
+++ MedlyChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedlyChallenge
+//
+//  Created by Alan Kantz on 11/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
