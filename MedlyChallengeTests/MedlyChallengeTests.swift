cabe8a04b127944ad615e65a776adb1dfe0b2c5b
Alan Kantz <alan.kantz@gmail.com>
2020-11-22 08:49:46 -0500

    When fed a configuration string, the countries service should be able to decode JSON from a local file.

--- MedlyChallengeTests/MedlyChallengeTests.swift
+++ MedlyChallengeTests/MedlyChallengeTests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 class MedlyChallengeTests: XCTestCase {
     
+    let timeout: TimeInterval = 10
+    
     lazy var bundle = Bundle(for: type(of: self))
     
     func test_TinyCountriesJSONDecodes() throws {
@@ -23,5 +25,24 @@ class MedlyChallengeTests: XCTestCase {
         
         // Then
         XCTAssertEqual(countries.count, 1)
+    }
+    
+    func test_ServiceCallToLocalJSONReturnsCountries() throws {
+        // Given
+        let service = CountryService.self
+        let jsonFileURL = try XCTUnwrap(bundle.url(forResource: "TinyCountries", withExtension: "json"))
+        let expectation = self.expectation(description: "The result will be set.")
+        
+        var result: Result<[Country], Error> = .failure(NSError(domain: "", code: .zero, userInfo: nil))
+        
+        // When
+        service.getAll(from: jsonFileURL.absoluteString) { outcome in
+            result = outcome
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: timeout)
+        let outcome = try result.get()
+        XCTAssertEqual(outcome.count, 1)
     }
 }
